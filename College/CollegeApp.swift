27e30c3e2905def62de09a2c6246e43c3b34dd69
thepsych0 <edserobian@yandex.ru>
2022-03-17 21:01:27 +0200

    Initial Commit

---
+++ College/CollegeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollegeApp.swift
+//  College
+//
+//  Created by ThePsych0 on 17.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CollegeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ College/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  College
+//
+//  Created by ThePsych0 on 17.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
